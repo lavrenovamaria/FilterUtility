f649ab5aa84b9624982b685a36732dbf0bd9250c
marialavrenova <lavrenova.maria.11@gmail.com>
2023-11-24 16:10:36 +0300

    add: Person.java

---
+++ src/main/java/com/example/filterutility/domain/Person.java
@@ -0,0 +1,50 @@
+package com.example.filterutility.domain;
+
+import java.time.LocalDate;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String gender;
+
+    private LocalDate birthdate;
+
+    public Person(String firstName, String lastName, String gender, LocalDate birthdate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.birthdate = birthdate;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+}
