fa32cc4e4a5551f9e588a65f8e71eb91f262efff
marialavrenova <lavrenova.maria.11@gmail.com>
2023-11-30 09:31:27 +0300

    add: post/preFilter interfaces

---
+++ src/main/java/com/example/filterutility/filter/PostFilter.java
@@ -0,0 +1,4 @@
+package com.example.filterutility.filter;
+
+public interface PostFilter<E> extends Filter<E>{
+}
---
+++ src/main/java/com/example/filterutility/filter/PreFilter.java
@@ -0,0 +1,4 @@
+package com.example.filterutility.filter;
+
+public interface PreFilter<E> extends Filter<E> {
+}
