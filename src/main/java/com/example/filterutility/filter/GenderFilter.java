1c1068c3a89737181b43dfdd852c9bde7416942f
marialavrenova <lavrenova.maria.11@gmail.com>
2023-11-30 11:59:24 +0300

    err: ParameterizedFilter is extends GenderFilter(?)

--- src/main/java/com/example/filterutility/filter/GenderFilter.java
+++ src/main/java/com/example/filterutility/filter/GenderFilter.java
@@ -1,8 +1,10 @@
 package com.example.filterutility.filter;
 
+import com.example.filterutility.domain.Gender;
+
 import java.util.List;
 
-public interface GenderFilter<T> extends PreFilter<T> {
+public interface GenderFilter<T> extends PreFilter<T>, ParameterizedFilter<T, Gender> {
     List<T> filter(List<T> items);
 }
 
