2dcccc360dc20f4c5f3aea1fa2be1d7df46b498f
marialavrenova <lavrenova.maria.11@gmail.com>
2023-12-03 20:42:50 +0300

    fix: make GenderFilter class

--- src/main/java/com/example/filterutility/filter/GenderFilter.java
+++ src/main/java/com/example/filterutility/filter/GenderFilter.java
@@ -2,6 +2,6 @@ package com.example.filterutility.filter;
 
 import java.util.List;
 
-public interface GenderFilter<T> extends PreFilter<T> {
-    List<T> filter(List<T> items);
+public class GenderFilter {
+
 }
